db43a09ace154b62a0960910767f7d717fa74556
MSI\Entahlah <putraadi664@gmail.com>
2018-10-05 08:45:28 -0800

    Tgl 5 Oktober 2018

---
+++ 4210161017_Putra Adi Wardana/DreamHitVersi2/Assets/Scripts/Database/Data.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour {
+    public static string _username, _email, _password;
+    public static int _status;
+    public static int _coin, _realMoney, _level;
+    public static int _id;
+
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
