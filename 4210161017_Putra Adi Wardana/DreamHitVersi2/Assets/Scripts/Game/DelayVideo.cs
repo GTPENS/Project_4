9757bec456eaf8d4d0528e3f5cc9872d25e9c4f5
MSI\Entahlah <putraadi664@gmail.com>
2018-10-15 10:11:17 -0800

    Polish Gameplay

---
+++ 4210161017_Putra Adi Wardana/DreamHitVersi2/Assets/Scripts/Game/DelayVideo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class DelayVideo : MonoBehaviour {
+
+    [SerializeField] VideoPlayer onlineVideo;
+    float timerPlay = 10;
+
+	// Use this for initialization
+	void Start () {
+        onlineVideo.Pause();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 4210161017_Putra Adi Wardana/DreamHitVersi2/Assets/Scripts/Game/Point.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour {
+
+    public static int coin = 0;
+    public static int MinusCoin = 0;
+
+    [SerializeField] int thisCoin;
+    [SerializeField] int thisMinus;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        thisCoin = coin;
+        thisMinus = MinusCoin;
+	}
+}
